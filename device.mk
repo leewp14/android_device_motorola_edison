6f508db8343e979c231f9ed7aaeb345b32f2f734
alteredlikeness <alteredlikeness@gmail.com>
2014-04-10 11:59:29 -0500

    remove unneeded files (gps.conf in common device; apns in rom vendor; recovery-kernel not needed)

--- device.mk
+++ device.mk
@@ -49,12 +49,10 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/media_profiles.xml:system/etc/media_profiles.xml \
-    $(DEVICE_FOLDER)/apns-conf.xml:system/etc/apns-conf.xml \
-    $(DEVICE_FOLDER)/gps.conf:system/etc/gps.conf \
+    $(DEVICE_FOLDER)/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_PACKAGES += fstab.mapphone_umts
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-$(call inherit-product-if-exists, vendor/motorola/edison/edison-vendor.mk)
+$(call inherit-product, vendor/motorola/edison/edison-vendor.mk)
 $(call inherit-product, vendor/motorola/omap4-common/omap4-common-imgtec-vendor-blobs.mk)
