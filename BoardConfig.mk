f7fb2b259e77df13bc67376c2f847148585c8196
lee.wp14 <lee.wp14@gmail.com>
2018-12-09 11:34:54 +0800

    Revert "Use CM PowerHAL" We already have TI OMAP PowerHAL. That will do the job well.
    This reverts commit 0ba66032f37658dcd0ed5c79fdcc89cf85837924.
    
    Conflicts:
    	tiffany/init.tiffany.rc
    
    Change-Id: Ib214b53cb805ba7d81b4a7edd224eb2fd6cb5db3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,9 +45,6 @@ TARGET_KERNEL_VARIANT_CONFIG := mapphone_edison_defconfig
 
 TARGET_USES_CUSTOM_INITFILES := true
 
-# CM PowerHAL
-TARGET_POWERHAL_VARIANT := cm
-
 # Specific recovery settings
 TARGET_RECOVERY_FSTAB := $(DEVICE_FOLDER)/fstab.mapphone_umts
 
